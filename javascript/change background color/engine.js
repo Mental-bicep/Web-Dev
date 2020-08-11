1e56bc4d99ba016002e308365ac51ad9f02c31eb
Ankit <ankitkumarmbill1@gmail.com>
2020-08-11 17:38:11 +0530

    improved ui

--- javascript/change background color/engine.js
+++ javascript/change background color/engine.js
@@ -1,14 +1,26 @@
-btn=document.getElementById("btn");
-let color=['#523906','#535204','#62760c','#cdb30c','#be5683','#ffe4e4','#900d0d','#006a71','#05dfd7','#05dfd7','#393e46'];
-let n=Math.floor(Math.random()*(color.length-1))
-document.body.style.backgroundColor=color[n];
+btn = document.getElementById("btn");
+let color = [
+    "#523906",
+    "#535204",
+    "#62760c",
+    "#cdb30c",
+    "#be5683",
+    "#ffe4e4",
+    "#900d0d",
+    "#006a71",
+    "#05dfd7",
+    "#05dfd7",
+    "#393e46",
+];
+let n = Math.floor(Math.random() * (color.length - 1));
+document.body.style.backgroundColor = color[n];
 
 // btn.style.backgroundColor=color[n];
-change=function (){
-    let n=Math.floor(Math.random()*(color.length-1))
-    console.log("value of n is \n"+ n);
-    document.body.style.backgroundColor=color[n];
+change = function () {
+    let n = Math.floor(Math.random() * (color.length - 1));
+    // console.log("value of n is \n"+ n);
+    document.body.style.backgroundColor = color[n];
     // btn.style.backgroundColor=color[n];
     // btn.style.borderColor='black';
-}
-btn.addEventListener("click",change);
+};
+btn.addEventListener("click", change);
