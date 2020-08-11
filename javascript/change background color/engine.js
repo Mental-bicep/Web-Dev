144022c472eb96ed188c3bb2bd1c31b34b75bc41
Ankit <ankitkumarmbill1@gmail.com>
2020-08-11 09:32:33 +0530

    fixed some bugs and improved gui for button

--- javascript/change background color/engine.js
+++ javascript/change background color/engine.js
@@ -1,13 +1,14 @@
 btn=document.getElementById("btn");
 let color=['#523906','#535204','#62760c','#cdb30c','#be5683','#ffe4e4','#900d0d','#006a71','#05dfd7','#05dfd7','#393e46'];
-// btn.addEventListener("onclick",change);
+let n=Math.floor(Math.random()*(color.length-1))
+document.body.style.backgroundColor=color[n];
+
+// btn.style.backgroundColor=color[n];
 change=function (){
     let n=Math.floor(Math.random()*(color.length-1))
     console.log("value of n is \n"+ n);
     document.body.style.backgroundColor=color[n];
+    // btn.style.backgroundColor=color[n];
+    // btn.style.borderColor='black';
 }
-btn.addEventListener("onclick",change);
-// btn.onclick=change;
-// btn.onclick=()=>{
-//     document.body.style.backgroundColor=color[2];
-// }
+btn.addEventListener("click",change);
